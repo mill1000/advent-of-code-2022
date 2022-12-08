fc4c45a7a9a1cd39269e5dedc90ed8417cf495c1
Tucker Kern <tucker.kern@broadcom.com>
2022-12-08 15:54:46 -0700

    Update template

--- template/solution.rb
+++ template/solution.rb
@@ -1,1 +1,14 @@
-#!/usr/bin/env ruby
+#! /usr/bin/env ruby
+
+require "optparse"
+
+SAMPLE_INPUT = "test"
+
+# Construct options parser
+parser = OptionParser.new do |opts|
+  opts.banner = "Usage: #{File.basename(__FILE__)} INPUT_FILE"
+end
+parser.parse!
+
+# Use sample input if no input file provided
+input = ARGV.empty? ? SAMPLE_INPUT : File.open(ARGV[0])
